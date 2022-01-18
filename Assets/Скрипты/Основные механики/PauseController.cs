c6fbbd7b596d2c418dc2ccf1861f51a3d167ca7c
iskanyeban <iskanyeban@gmail.com>
2022-01-18 22:13:12 +0500

    180122.20:12
    +Пауза

---
+++ Assets/Скрипты/Основные механики/PauseController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PauseController : MonoBehaviour
+{
+    public GameObject pauseBox;
+
+    bool isPaused;
+
+    void Update()
+    {
+        pauseBox.SetActive(isPaused);
+        Time.timeScale = isPaused ? 0 : 1;
+
+        if (Input.GetKeyDown(KeyCode.Escape)) isPaused = !isPaused; 
+    }
+
+    public void Continue() => isPaused = false;
+
+    public void Quit() => Application.Quit();
+}
