cb882f1f7c74d1b6509ce69774349c74a337f81f
Rostislav <rostimon@mail.ru>
2022-01-23 18:30:37 +0700

    230122.14:30
    AI рефакторинг и легкий оптимиз
    Добавил Gizmos поля зрения врага

--- Assets/Скрипты/Передвижение/AI.cs
+++ Assets/Скрипты/Передвижение/AI.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
+using System.Collections;
 using Pathfinding;
 
 //Скрипт ИИ
 public class AI : MonoBehaviour
 {
-    public LayerMask obstacles, player; //Слой, видимый для ИИ
+    private LayerMask obstacles = 1 << 3; //Слой с препятствиями
+    private LayerMask player = 1 << 6; //Слой с игроком
+    //public float speed; //Скорость
     public float seeRange; //Дальность зрения
     public float spotSpeed; //Скорость движения(При погоне)
     public Vector2 direction = Vector2.right;
@@ -12,118 +15,150 @@ public class AI : MonoBehaviour
     [Header("Patrol AI")] public bool isPatrol; //Является ли данный ИИ патрульным     
     public float patrolSpeed; //Скорость движения(При патрулировании)
     public Vector2[] path; //Массив пути(для патрульного ИИ)
-    [HideInInspector] public bool isSee; //Видит ли ИИ игрока?
-    [HideInInspector] public Vector2 target; //Позиция цели
+    [HideInInspector] public bool isPlayerSeen; //Видит ли ИИ игрока?
+    [HideInInspector] public Transform currentTarget; //Трансформ цели
 
     AnimationMovementController anim; //Контроллер анимаций
-    AILerp ai; //Скрипт поиска пути
-    bool patr; //Патрулирует ли ИИ?
-    int currWay = 0; //Текущий путь из массива позиций
-    float swTime; //Время ожидания(для патруля)
+    IAstarAI ai; //Скрипт поиска пути
+
+    [SerializeField] private bool patrolling; //Патрулирует ли ИИ?
+    private int currWay = 0; //Текущий путь из массива позиций
+    private float currTime; //Таймер
+    private float swTime; //Время ожидания(для патруля)
 
     void Awake()
     {
         //Получаем скрипт поиска пути и контроллер анимаций
         ai = GetComponent<AILerp>();
         anim = GetComponent<AnimationMovementController>();
-        patr = isPatrol;
+        patrolling = isPatrol;
+    }
+
+    private bool CanSeePlayer()
+    {
+        var check = Physics2D.OverlapCircle(transform.position, seeRange, player); //Проверяем наличие игрока в радиусе поиска
+
+        if (check == null) 
+            return false;
+
+        //Проверяем угол между игроком и ИИ
+        Vector3 dirToPlayer = (check.transform.position - transform.position).normalized;
+        if (Mathf.Acos(Vector2.Dot(direction, dirToPlayer)) * Mathf.Rad2Deg < viewAngle * .5f)
+        {
+            //Если угол укладывается в поле зрения, то мы проверяем на наличие препятствий между ИИ и игроком
+            if (!Physics2D.Linecast(transform.position, check.transform.position, obstacles))
+            {
+                //Препятствий нет - начинаем преследовать игрока             
+                currentTarget = check.transform;
+                direction = dirToPlayer;
+                return true;
+            }
+        }
+
+        return false;
     }
 
     void Update()
     {
-        var check = Physics2D.OverlapCircle(transform.position, seeRange, player); //Проверяем наличие игрока в радиусе поиска
-
-        if (check != null)
-        {
-            //Проверяем угол между игроком и ИИ
-            if (Mathf.Acos(Vector2.Dot(direction, (check.transform.position - transform.position).normalized)) * Mathf.Rad2Deg < viewAngle * .5f) 
-            {
-                //Если угол укладывается в поле зрения, то мы проверяем на наличие препятствий между ИИ и игроком
-                if (!Physics2D.Raycast(transform.position, (check.transform.position - transform.position).normalized,
-                (check.transform.position - transform.position).magnitude, obstacles))
-                {
-                    //Препятствий нет - начинаем преследовать игрока             
-                    target = check.transform.position;
-                    direction = (check.transform.position - transform.position).normalized;
-                    isSee = true;
-                }
-                //Препятствие есть - ИИ не видит игрока
-                else isSee = false;
-            }
-            //Угол не укладывается - ИИ не видит игрока
-            else isSee = false;
-        }
-
-        //Если каким-то образом ИИ думает что видит игрока, но его нет в зоне досигаемости, то ИИ больше игрока не видит
-        else if (isSee) isSee = false;
+        isPlayerSeen = CanSeePlayer();
 
         //Если ИИ уже как [2;4] секунды не видел игрока, то(Если ИИ патрульный) начинаем патрулировать нашу зону патруля
-        if (ai.reachedEndOfPath && !isSee && !patr && isPatrol) Invoke("StartPatroling", Random.Range(2, 4));
+        if (ai.reachedEndOfPath && !isPlayerSeen && !patrolling && isPatrol) 
+            StartCoroutine(LostPlayer(Random.Range(2, 4)));
 
         //Если ИИ видит игрока
-        if (isSee)
+        if (isPlayerSeen)
         {
-            patr = false; //Отключаем патруль
-            currWay = 0;             
-            swTime = float.PositiveInfinity;
-            ai.speed = spotSpeed; //Меняем ему скорость
-            ai.destination = target; //Меняем ему цель на игрока
-            ai.SearchPath(); //Ищем путь до игрока
+            ChaseTarget(currentTarget);
         }
-
         //Если ИИ не видит игрока, но он патрульный
-        else if (patr)
+        else if (patrolling)
         {
-            ai.speed = patrolSpeed; //Меняем скорость на обычную
-            var search = false; //Надо ли нам искать путь?
-
-            //Если ИИ достиг конца пути, не ищет путь и его время ожидания равно бесконечности, 
-            //то мы назначаем ему время после которого ему надо будет идти к другой точке патруля
-            if (ai.reachedEndOfPath && !ai.pathPending && float.IsPositiveInfinity(swTime)) swTime = Time.time + Random.Range(.5f, 6);
-
-            //Если пора искать путь
-            if (Time.time >= swTime)
-            {
-                currWay++; //Обновляем путь
-                search = true; //Да, нам надо искать путь
-                swTime = float.PositiveInfinity; //Делаем время ожидания недосигаемым
-            }
-
-            currWay %= path.Length; //Вычисляем остаток от деления текущего пути на длины массива путей, чтобы текущий путь не превышал кол-во путей
-            ai.destination = path[currWay]; //Назначаем ИИ путь
-
-            if (search) ai.SearchPath(); //Если надо ищем этот самый путь
-
+            PatrolArea();
         }
 
         //Если ИИ ничего не видит, и направление его пути не равно нулю, то направление его зрения должно быть равно направлению его движения
-        if (!isSee && ai.velocity != Vector3.zero) direction = ai.velocity.normalized;
+        if (!isPlayerSeen && ai.velocity != Vector3.zero) direction = ai.velocity.normalized;
 
         //Отправляем контроллеру анимаций данные передвижениях ИИ
         anim.isWalk = !ai.reachedEndOfPath;
         anim.direction = direction;
     }
 
-    //Система реагирования на скриптовые шумы
-    public void NoiseDistraction(Vector2 noise)
+    private void PatrolArea()
     {
-        patr = false; //Отключаем патруль      
-        ai.speed = spotSpeed; //Меняем скорость
-        ai.destination = noise; //Назначем цель на место шума   
-        ai.SearchPath(); //Ищем путь до места шума
+        ai.maxSpeed = patrolSpeed; //Меняем скорость на обычную
+
+        //Если ИИ достиг конца пути, не ищет путь и его время ожидания не равно бесконечности, 
+        //то мы назначаем ему время после которого ему надо будет идти к другой точке патруля
+        if (ai.reachedEndOfPath && !ai.pathPending && float.IsPositiveInfinity(swTime))
+        {
+            swTime = Random.Range(.5f, 6f);
+            currTime = 0f;
+        }
+
+
+        //Если пора искать путь
+        if (currTime >= swTime)
+        {
+            if((Vector2) ai.destination == path[currWay]){
+                currWay++; //Обновляем путь
+                currWay %= path.Length; //Вычисляем остаток от деления текущего пути на длины массива путей, чтобы текущий путь не превышал кол-во путей
+            }
+            
+            ai.destination = path[currWay]; //Назначаем ИИ путь
+            ai.SearchPath(); //Если надо ищем этот самый путь
+            swTime = float.PositiveInfinity; //Делаем время ожидания недосигаемым
+        }
+
+        currTime += Time.deltaTime; //Time.time неюзабилен после 9ти часов работы приложения, поэтому заменил на вариант с deltaTime
+    }
+
+    private void ChaseTarget(Transform target)
+    {
+        patrolling = false; //Отключаем патруль
+        swTime = float.PositiveInfinity;
+        currentTarget = target;
+        ai.maxSpeed = spotSpeed; //Меняем ему скорость
+        ai.destination = target.position; //Меняем ему цель на объект
+        ai.SearchPath(); //Ищем путь до объекта
+    }
+
+    //Система реагирования на скриптовые шумы
+    public void NoiseDistraction(Transform noise)
+    {
+        ChaseTarget(noise);
     }
 
     //Функция начала путрлирования, после того как ИИ некоторое время не видел игрока
-    void StartPatroling() => patr = true;
+    IEnumerator LostPlayer(float timeTillPatrolling)
+    {
+        yield return new WaitForSeconds(timeTillPatrolling);
+        patrolling = true;
+    }
 
     //Для наглядности рисуем точки патруля желтыми кружками
     void OnDrawGizmos() 
     {
+        //Границы зрения
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, Quaternion.Euler(0f, 0f, viewAngle/2) * direction * seeRange);
+        Gizmos.DrawRay(transform.position, Quaternion.Euler(0f, 0f, -viewAngle/2) * direction * seeRange);
+
         if (isPatrol)
         {
-            Gizmos.color = Color.yellow;
-
-            foreach (var i in path) Gizmos.DrawWireSphere(i, .2f);
+            Gizmos.color = Color.green;
+            Vector2 lastPoint = path[0];
+            foreach (var currentPoint in path)
+            {
+                Gizmos.DrawLine(lastPoint,currentPoint);
+                Gizmos.DrawWireSphere(currentPoint, .2f);
+                lastPoint = currentPoint;
+                Gizmos.color = Color.yellow;
+            }
+            Gizmos.DrawLine(lastPoint, path[0]);
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(lastPoint, .2f);
         }
     }
 }
