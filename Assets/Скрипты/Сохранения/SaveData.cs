9c37415b0f62b6dbaa24f23179a4e06132c14ff0
iskanyeban <iskanyeban@gmail.com>
2022-10-23 13:06:00 +0500

    версию поменял

--- Assets/Скрипты/Сохранения/SaveData.cs
+++ Assets/Скрипты/Сохранения/SaveData.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 [Serializable]
 public class SaveData
 {
-    public const string currentVersion = "bt 10";
+    public const string currentVersion = "bt 11";
     public string version = currentVersion;
 
     public int level;
