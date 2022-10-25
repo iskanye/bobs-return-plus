1d7eba6279c8cec27d3bcb18348abb570ffc317a
Rostislav <rostimon@mail.ru>
2022-10-26 04:39:28 +0700

    рефакторинг арбалета и триггера
    Оптимизировал арбалет, добавил возможность триггеру игнорировать коллайдер или триггер

--- Assets/Скрипты/Основные механики/Cannon.cs
+++ Assets/Скрипты/Основные механики/Cannon.cs
@@ -1,69 +1,75 @@
 using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider2D))]
 public class Cannon : MonoBehaviour
 {
-    public GameObject bullet;
-    public float distance;
-    public Vector2 direction;
-    public Vector2 bulletOffset;
-    public Vector2 offset;
-    public LayerMask player;
-    public float force;
-    public bool isReloadable;
-    public float reloadDelay;
-    public Animator[] animators;
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private float distance;
+    [SerializeField] private Vector2 direction;
+    [SerializeField] private Vector2 bulletOffset;
+    [SerializeField] private Vector2 offset;
+    [SerializeField] private LayerMask player;
+    [SerializeField] private float force;
+    [SerializeField] private bool isReloadable;
+    [SerializeField] private float reloadDelay;
+    [SerializeField] private Animator[] animators;
 
-    public bool HaveShoted { set; get; }
-
-    float swTime;
-    PropertyHolder prop;
-
-    void Awake() =>
-        prop = GetComponent<PropertyHolder>();
+    private bool haveShot;
     
-    void Update()
+    private float swTime;
+    private PropertyHolder prop;
+    
+    private static readonly int Shoot = Animator.StringToHash("Shoot");
+    private static readonly int DirX = Animator.StringToHash("DirX");
+    private static readonly int DirY = Animator.StringToHash("DirY");
+    
+    private BoxCollider2D trigger;
+    
+    void Awake()
     {
+        prop = GetComponent<PropertyHolder>();
         foreach (var i in animators)
         {
-            i.SetFloat("DirX", direction.x);
-            i.SetFloat("DirY", direction.y);
+            i.SetFloat(DirX, direction.x);
+            i.SetFloat(DirY, direction.y);
         }
+    }
 
-        if (Physics2D.Raycast(transform.position + (Vector3)offset, direction, distance, player))
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if ((1<<col.gameObject.layer) != player.value)
+            return;
+        
+        bool raycastHit2D = Physics2D.Raycast(transform.position + (Vector3)offset, direction, distance, player);
+        if (raycastHit2D)
         {
-            if (!isReloadable && !HaveShoted)
+            if (!haveShot || (Time.time >= swTime && isReloadable))
             {
                 var bull = Instantiate(bullet, transform.position + (Vector3)bulletOffset, Quaternion.identity);
                 bull.GetComponent<Rigidbody2D>().AddForce(direction * force, ForceMode2D.Impulse);
-                HaveShoted = true;
-
+                haveShot = true;
+                swTime = Time.time + reloadDelay;
+                
                 foreach (var i in animators)
-                    i.SetTrigger("Shoot");
+                    i.SetTrigger(Shoot);
 
                 if (prop != null)
                     prop.property = true;
             }
-
-            else if (isReloadable && float.IsPositiveInfinity(swTime))
-            {
-                var bull = Instantiate(bullet, transform.position + (Vector3)bulletOffset, Quaternion.identity);
-                bull.GetComponent<Rigidbody2D>().AddForce(direction * force, ForceMode2D.Impulse);
-                swTime = Time.time + reloadDelay;
-
-                foreach (var i in animators)
-                    i.SetTrigger("Shoot");
-            }
         }
+    }
 
-        if (Time.time >= swTime)
-            swTime = float.PositiveInfinity;
+    private void OnValidate()
+    {
+        trigger = GetComponent<BoxCollider2D>();
+        trigger.offset = offset + Vector2.right*distance/2;
+        trigger.size = new Vector2(distance, 0.01f);
+        trigger.isTrigger = true;
     }
 
     void OnDrawGizmos()
     {
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position + (Vector3)bulletOffset, .1f);
-        Gizmos.color = Color.green;
-        Gizmos.DrawRay(transform.position + (Vector3)offset, direction * distance);
     }
 }
--- Assets/Скрипты/Тригеры/Trigger.cs
+++ Assets/Скрипты/Тригеры/Trigger.cs
@@ -3,24 +3,36 @@ using UnityEngine;
 public class Trigger : ActionBase
 {
     public UnityEngine.Events.UnityEvent<GameObject> onExit;
-    public LayerMask playerMask; 
-    public bool isOnStay;
-
+    [SerializeField] private LayerMask playerMask; 
+    [SerializeField] private bool isOnStay;
+    [SerializeField] private bool TriggerNotExpected;
+    [SerializeField] private bool ColliderNotExpected;
+    
     void OnTriggerEnter2D(Collider2D c) 
     {
-        if (action != null && ((1 << c.gameObject.layer) | playerMask) == playerMask && !isOnStay) 
+        if (IsTriggering(c) && !isOnStay) 
             action.Invoke(c.gameObject); 
     }
 
     void OnTriggerStay2D(Collider2D c) 
     {
-        if (action != null && ((1 << c.gameObject.layer) | playerMask) == playerMask && isOnStay) 
+        if (IsTriggering(c) && isOnStay) 
             action.Invoke(c.gameObject); 
     }
 
     void OnTriggerExit2D(Collider2D c)
     {
-        if (action != null && ((1 << c.gameObject.layer) | playerMask) == playerMask)
+        if (IsTriggering(c))
             onExit.Invoke(c.gameObject);
+    }
+
+    bool IsTriggering(Collider2D c)
+    {
+        if (action != null && ((1 << c.gameObject.layer) & playerMask) != 0 && (c.isTrigger & !TriggerNotExpected || !c.isTrigger & !ColliderNotExpected))
+        {
+            return true;
+        }
+
+        return false;
     }
 }
