35212c15d07f62e139b1b5c9a453d05c6cdae8b1
Искандер <84191128+iskanye@users.noreply.github.com>
2022-11-21 16:35:31 +0500

    отделил разброс физ тел от скрипта обьекта с прочностью
    переделал бочки, добавил траву, переделал камеру

--- Assets/Скрипты/Основные механики/PenetratingObject.cs
+++ Assets/Скрипты/Основные механики/PenetratingObject.cs
@@ -3,19 +3,10 @@
 public class PenetratingObject : MonoBehaviour
 {
     public ObjectType type;
-    public Vector3 particlesOffset;
-    public float particlesForce;
-    public Rigidbody2D[] destroyParticles;
     public UnityEngine.Events.UnityEvent<GameObject> onPenetrate;
 
     public ObjectType Durability { set => type = value; }
 
-    public void Penetrate() 
-    {
-        if (destroyParticles != null)
-            foreach (var i in destroyParticles)
-                Instantiate(i, transform.position + particlesOffset, Quaternion.identity).AddForce(particlesForce * (Vector2)Random.onUnitSphere, ForceMode2D.Impulse);
-
+    public void Penetrate() =>
         onPenetrate.Invoke(gameObject);
-    }
 }
---
+++ Assets/Скрипты/Основные механики/SpreadRigidbodies.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class SpreadRigidbodies : MonoBehaviour
+{    
+    public Vector3 offset;
+    public float force;
+    public Rigidbody2D[] rigidbodies;
+
+    public void Spread() 
+    {
+        foreach (var i in rigidbodies)
+            Instantiate(i, transform.position + offset, Quaternion.identity).AddForce(force * Random.onUnitSphere, ForceMode2D.Impulse);
+    }
+}
